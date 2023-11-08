70dd48b632e24f91677d182ad8ca7caa5788318b
Toan ne <toan70868@gmail.com>
2023-11-08 16:25:30 +0700

    responsive header new

--- view/header.php
+++ view/header.php
@@ -103,10 +103,12 @@
                                         <div class="language ht-language">
                                             <ul class="ht-setting-list">
                                                 <li class="active">
-                                                    <a href="#"><img src="./view/layout/images/menu/flag-icon/1.jpg" alt>English</a>
+                                                    <a href="#"><img src="./view/layout/images/menu/flag-icon/1.jpg"
+                                                            alt>English</a>
                                                 </li>
                                                 <li>
-                                                    <a href="#"><img src="./view/layout/images/menu/flag-icon/2.jpg" alt>Français</a>
+                                                    <a href="#"><img src="./view/layout/images/menu/flag-icon/2.jpg"
+                                                            alt>Français</a>
                                                 </li>
                                             </ul>
                                         </div>
@@ -138,7 +140,8 @@
                             <!-- Begin Header Middle Searchbox Area -->
                             <form action="#" class="hm-searchbox">
                                 <input type="text" placeholder="Enter your search key ...">
-                                <button class="li-btn" type="submit"><i style="color: white;" class="fa fa-search"></i></button>
+                                <button class="li-btn" type="submit"><i style="color: white;"
+                                        class="fa fa-search"></i></button>
                             </form>
                             <!-- Header Middle Searchbox Area End Here -->
                             <!-- Begin Header Middle Right Area -->
@@ -148,7 +151,8 @@
                                     <li class="hm-wishlist">
                                         <a href="wishlist.html">
                                             <span class="cart-item-count wishlist-item-count">0</span>
-                                            <i style="padding-top: 6px; color: red; font-size: 30px;" class="fa fa-heart-o"></i>
+                                            <i style="padding-top: 6px; color: red; font-size: 30px;"
+                                                class="fa fa-heart-o"></i>
                                         </a>
                                     </li>
 
@@ -167,7 +171,8 @@
                                             <ul class="minicart-product-list">
                                                 <li>
                                                     <a href="single-product.html" class="minicart-product-image">
-                                                        <img src="./view/layout/images/product/small-size/5.jpg" alt="cart products">
+                                                        <img src="./view/layout/images/product/small-size/5.jpg"
+                                                            alt="cart products">
                                                     </a>
                                                     <div class="minicart-product-details">
                                                         <h6><a href="single-product.html">Aenean eu tristique</a></h6>
@@ -179,7 +184,8 @@
                                                 </li>
                                                 <li>
                                                     <a href="single-product.html" class="minicart-product-image">
-                                                        <img src="./view/layout/images/product/small-size/6.jpg" alt="cart products">
+                                                        <img src="./view/layout/images/product/small-size/6.jpg"
+                                                            alt="cart products">
                                                     </a>
                                                     <div class="minicart-product-details">
                                                         <h6><a href="single-product.html">Aenean eu tristique</a></h6>
@@ -192,7 +198,8 @@
                                             </ul>
                                             <p class="minicart-total">Tổng: <span>£80.00</span></p>
                                             <div class="minicart-button">
-                                                <a href="shopping-cart.html" class="li-button li-button-fullwidth li-button-dark">
+                                                <a href="shopping-cart.html"
+                                                    class="li-button li-button-fullwidth li-button-dark">
                                                     <span>View Full Cart</span>
                                                 </a>
                                                 <a href="checkout.html" class="li-button li-button-fullwidth">
@@ -224,7 +231,7 @@
                                     </li>
                                     <!-- Header Mini Cart Area End Here -->
                                 </ul>
-                                <ul style="padding-top: 15px;">Đăng nhập</ul>
+                                <ul class="minicart-product-list__register" style="padding-top: 15px;">Đăng nhập</ul>
                             </div>
                             <!-- Header Middle Right Area End Here -->
                         </div>
--- view/home.php
+++ view/home.php
@@ -74,8 +74,10 @@
                   <div class="col-lg-12">
                       <div class="li-product-tab">
                           <ul class="nav li-product-menu">
-                              <li><a class="active" data-toggle="tab" href="#li-new-product"><span>Hàng mới về</span></a></li>
-                              <li><a data-toggle="tab" href="#li-bestseller-product"><span>Sản phẩm bán chạy</span></a></li>
+                              <li><a class="active" data-toggle="tab" href="#li-new-product"><span>Hàng mới
+                                          về</span></a></li>
+                              <li><a data-toggle="tab" href="#li-bestseller-product"><span>Sản phẩm bán chạy</span></a>
+                              </li>
                               <li><a data-toggle="tab" href="#li-featured-product"><span>Sản phẩm nổi bật</span></a>
                               </li>
                           </ul>
@@ -113,7 +115,8 @@
                                                       </ul>
                                                   </div>
                                               </div>
-                                              <h4><a class="product_name" href="single-product.html">iPhone 15 Pro Max 256GB</a></h4>
+                                              <h4><a class="product_name" href="single-product.html">iPhone 15 Pro Max
+                                                      256GB</a></h4>
                                               <div class="price-box">
                                                   <span class="new-price">31.000.000VNĐ</span>
                                               </div>
@@ -1295,6 +1298,120 @@
       </section>
       <!-- Li's Laptop Product Area End Here -->
       <!-- Begin Li's TV & Audio Product Area -->
+
+      <!-- Begin Featured Product With Banner Area -->
+      <div class="featured-pro-with-banner mt-sm-5 pb-sm-10 mt-xs-5 pb-xs-10">
+          <div class="container">
+              <div class="row">
+                  <!-- Begin Li's Featured Banner Area -->
+                  <div class="col-lg-3 text-center">
+                      <div class="single-banner featured-banner">
+                          <a href="#">
+                              <img src="images/banner/featured-banner.jpg" alt="Li's Featured Banner">
+                          </a>
+                      </div>
+                  </div>
+                  <!-- Li's Featured Banner Area End Here -->
+                  <!-- Begin Featured Product Area -->
+                  <div class="col-lg-9">
+                      <div class="featured-product pt-sm-30 pt-xs-30">
+                          <div class="li-section-title">
+                              <h2>
+                                  <span>Featured Products</span>
+                              </h2>
+                          </div>
+                          <div class="row">
+                              <div class="featured-product-active owl-carousel">
+                                  <div class="featured-product-bundle">
+                                      <div class="featured-pro-wrapper mb-30 mb-sm-25">
+                                          <div class="product-img">
+                                              <a href="product-details.html">
+                                                  <img alt src="images/featured-product/1.jpg">
+                                              </a>
+                                          </div>
+                                          <div class="featured-pro-content">
+                                              <div class="product-review">
+                                                  <h5 class="manufacturer">
+                                                      <a href="shop-left-sidebar.html">Studio Design</a>
+                                                  </h5>
+                                              </div>
+                                              <div class="rating-box">
+                                                  <ul class="rating">
+                                                      <li><i class="fa fa-star-o"></i></li>
+                                                      <li><i class="fa fa-star-o"></i></li>
+                                                      <li><i class="fa fa-star-o"></i></li>
+                                                      <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                      <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                  </ul>
+                                              </div>
+                                              <h4><a class="featured-product-name" href="single-product.html">Mug Today
+                                                      is a good day</a></h4>
+                                              <div class="featured-price-box">
+                                                  <span class="new-price new-price-2">$71.80</span>
+                                                  <span class="old-price">$77.22</span>
+                                                  <span class="discount-percentage">-7%</span>
+                                              </div>
+                                              <div class="featured-product-action">
+                                                  <ul class="add-actions-link">
+                                                      <li class="add-cart active"><a href="#">Add to cart</a></li>
+                                                      <li><a class="links-details" href="single-product.html"><i
+                                                                  class="fa fa-heart-o"></i></a></li>
+                                                      <li><a class="quick-view" data-toggle="modal"
+                                                              data-target="#exampleModalCenter" href="#"><i
+                                                                  class="fa fa-eye"></i></a></li>
+                                                  </ul>
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="featured-pro-wrapper">
+                                          <div class="product-img">
+                                              <a href="product-details.html">
+                                                  <img alt src="images/featured-product/1.jpg">
+                                              </a>
+                                          </div>
+                                          <div class="featured-pro-content">
+                                              <div class="product-review">
+                                                  <h5 class="manufacturer">
+                                                      <a href="shop-left-sidebar.html">Studio Design</a>
+                                                  </h5>
+                                              </div>
+                                              <div class="rating-box">
+                                                  <ul class="rating">
+                                                      <li><i class="fa fa-star-o"></i></li>
+                                                      <li><i class="fa fa-star-o"></i></li>
+                                                      <li><i class="fa fa-star-o"></i></li>
+                                                      <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                      <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                  </ul>
+                                              </div>
+                                              <h4><a class="featured-product-name" href="single-product.html">Mug Today
+                                                      is a good day</a></h4>
+                                              <div class="featured-price-box">
+                                                  <span class="new-price">$71.80</span>
+                                              </div>
+                                              <div class="featured-product-action">
+                                                  <ul class="add-actions-link">
+                                                      <li class="add-cart active"><a href="#">Add to cart</a></li>
+                                                      <li><a class="links-details" href="single-product.html"><i
+                                                                  class="fa fa-heart-o"></i></a></li>
+                                                      <li><a class="quick-view" data-toggle="modal"
+                                                              data-target="#exampleModalCenter" href="#"><i
+                                                                  class="fa fa-eye"></i></a></li>
+                                                  </ul>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <!-- Featured Product Area End Here -->
+              </div>
+          </div>
+      </div>
+
+
       <section class="product-area li-laptop-product li-tv-audio-product pb-45">
           <div class="container">
               <div class="row">
