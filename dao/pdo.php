c25080ab1e484d6a9ab316c605e13858332e9be1
Phùng Nguyễn Nguyên Anh <anhpnnps34177@fpt.edu.vn>
2023-11-12 20:24:32 +0700

    Revert "Diễm thêm hình"
    This reverts commit 4f753c37948317222b5e8b109790ce384946e63e.
    
    # Conflicts:
    #	dao/pdo.php
    #	dao/sanpham.php

--- dao/pdo.php
+++ dao/pdo.php
@@ -5,7 +5,7 @@
  */
 function pdo_get_connection()
 {
-    $dburl = "mysql:host=localhost;dbname=da1;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=duan1;charset=utf8";
     $username = 'root';
     $password = '';
 
