88ed039174fbfc5d63c13f2fd287a9f08d7ed80a
BDgDiem <jodd23855@gmail.com>
2023-11-15 10:56:06 +0700

    Revert "diem"
    This reverts commit 86c51e615c4f932b5d39182c94278d545f272e3d.

--- dao/sanpham.php
+++ dao/sanpham.php
@@ -79,11 +79,15 @@ function show_SP($dssp)
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <<<<<<< Updated upstream
                 <a href="'.$link.'">
 =======
                 <a href="index.php?pg=productdetail">
 >>>>>>> Stashed changes
+=======
+                <a href="index.php?pg=productdetail">
+>>>>>>> parent of 86c51e6 (diem)
 =======
                 <a href="index.php?pg=productdetail">
 >>>>>>> parent of 86c51e6 (diem)
@@ -351,6 +355,7 @@ function show_dssp($dssp_sp)
 }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> Stashed changes
 
 function get_dssp($limit)
@@ -409,6 +414,8 @@ function show_dssp($dssp_sp)
     }
     return $showhtml;
 }
+=======
+>>>>>>> parent of 86c51e6 (diem)
 =======
 >>>>>>> parent of 86c51e6 (diem)
 =======
