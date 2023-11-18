a84611d30483317c8122f5ef0052efeb36cf26a0
Toan ne <toan70868@gmail.com>
2023-11-18 12:15:38 +0700

    Revert "Toan"
    This reverts commit 397362ce92c5ff96ff8cbc21e109bb2e9a36a1ce.

--- view/login_register.php
+++ view/login_register.php
@@ -1,6 +1,7 @@
 <div class="page-section mb-60">
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-md-12 col-xs-12 col-lg-6 mb-30">
@@ -121,6 +122,46 @@
                                     </div>
                                 </div>
 >>>>>>> parent of 397362c (Toan)
+=======
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-xs-12 col-lg-6 mb-30">
+                            <!-- Login Form s-->
+                            <form action="index.php?pg=login" method="post">
+                                <div class="login-form">
+                                    <h4 class="login-title">Đăng nhập</h4>
+                                    <div class="row">
+                                        <div class="col-md-12 col-12 mb-20">
+                                            <label>Tên đăng nhập*</label>
+                                            <input class="mb-0" type="text" id="username" name="username" placeholder="Hãy nhập tên đăng nhập">
+                                        </div>
+                                        <div class="col-12 mb-20">
+                                            <label>Mật khẩu</label>
+                                            <input class="mb-0" type="password" id="password" name="password" placeholder="Mật khẩu của bạn">
+                                        </div>
+                                        <h9 style="color:red">
+                                            <?php
+                                            if(isset($_SESSION['tb_dangnhap'])&&($_SESSION['tb_dangnhap']!="")){
+                                                echo $_SESSION['tb_dangnhap'];
+                                                unset ($_SESSION['tb_dangnhap']);
+                                            }  
+                                            ?>
+                                        </h9>
+                                        <div class="col-md-8">
+                                            <div class="check-box d-inline-block ml-0 ml-md-2 mt-10">
+                                                <input type="checkbox" id="remember_me">
+                                                <label for="remember_me">Ghi nhớ tôi</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 mt-10 mb-20 text-left text-md-right">
+                                            <a href="index.php?pg=ft_password">Bạn đã quên mật khẩu?</a>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <input class="name" name="dangnhap" type="submit" value="Đăng nhập">
+                                        </div>
+                                    </div>
+                                </div>
+>>>>>>> parent of 397362c (Toan)
                             </form>
                         </div>
                         <div class="col-sm-12 col-md-12 col-lg-6 col-xs-12">
@@ -159,7 +200,10 @@
                                 </div>
                             </form>
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 79402d2bea044d67fd78ecfb0d4c714dcaae1148
+=======
+>>>>>>> parent of 397362c (Toan)
 =======
 >>>>>>> parent of 397362c (Toan)
                         </div>
