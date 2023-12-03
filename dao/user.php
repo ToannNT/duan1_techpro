6879adc76e503088ad26f904a4fc20bb93381fa1
Toan ne <toan70868@gmail.com>
2023-12-03 21:56:26 +0700

    Toan
    Fix conflict

--- dao/user.php
+++ dao/user.php
@@ -75,9 +75,12 @@ function  update_pass_user($newpassword, $id)
 
 //     return ""; // Return an empty string if no image is found
 // }
-function getLatestImageFromUser($id) {
+function getLatestImageFromUser($id)
+{
+
+
     // Truy vấn SQL để lấy thông tin ảnh gần nhất từ bảng user
-    $sql = "SELECT hinh FROM userWHERE id = id ORDER BY upload_date DESC LIMIT 1";
+    $sql = "SELECT image_path FROM user_images WHERE user_id = :user_id ORDER BY upload_date DESC LIMIT 1";
 }
 
 
