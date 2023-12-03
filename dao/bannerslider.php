939969b087a7f41014fc71f5a7ab40129eb90bbe
Toan ne <toan70868@gmail.com>
2023-12-03 22:28:37 +0700

    Revert "themsuaxoabanner"
    This reverts commit 883d1dd06afa6a2a7aca08aed7c30e2d6df07597.

--- dao/bannerslider.php
+++ dao/bannerslider.php
@@ -1,24 +1,46 @@
 <?php
 require_once 'pdo.php';
 ///banner
-function db_banner(){
+function db_banner()
+{
     $sql = "SELECT * FROM banner ORDER BY id DESC";
     return pdo_query($sql);
 }
-function db_slider(){
+function db_slider()
+{
     $sql = "SELECT * FROM slider ORDER BY id DESC";
     return pdo_query($sql);
 }
 //tạo mới banner
-function insert_banner($stt, $mota, $img){
+function insert_banner($stt, $mota, $img)
+{
     $sql = "INSERT INTO banner(stt, mota, img) VALUES(?,?,?)";
     pdo_execute($sql, $stt, $mota, $img);
 }
 //kết thúc tạo mới banner
 // xoá banner
-function del_banner($id){
+function del_banner($id)
+{
     $sql = "DELETE FROM banner WHERE  id=?";
     pdo_execute($sql, $id);
+}
+// updatebanner
+function update_banner($stt, $mota, $img, $idbn)
+{
+    $sql = "UPDATE banner SET stt=?, mota=?, img=? WHERE id=?";
+    pdo_execute($sql, $stt, $mota, $img, $idbn);
+}
+function showup_banner($idbn)
+{
+    $sql = "SELECT * FROM banner WHERE id=?";
+    return pdo_query_one($sql, $idbn);
+}
+
+
+function update_status($trangthai, $id)
+{
+    $sql = "UPDATE banner SET trangthai=? WHERE id=?";
+    pdo_execute($sql, $trangthai, $id);
 }
 // kết thúc xoá banner
 ///endbanner
