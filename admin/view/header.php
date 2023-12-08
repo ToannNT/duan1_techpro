1b8687b8ffea51d2ddf5b83235f81532628f3019
Toan ne <toan70868@gmail.com>
2023-12-08 10:39:54 +0700

    Revert "Merge branch 'main' of https://github.com/ToannNT/duan1_techpro"
    This reverts commit 52ddabb7914e74d506f3e2c99a0f2dd65b8b3a60, reversing
    changes made to 5b9af7b6b32548a3ab541e881d41b3bd757047fe.

---
+++ admin/view/header.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title> Quản trị Admin</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Main CSS-->
+        <link rel="stylesheet" type="text/css" href="view/layouts/css/main1.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+        <!-- or -->
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+        <!-- Font-icon css-->
+        <link rel="stylesheet" type="text/css"
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+        <script src="http://code.jquery.com/jquery.min.js" type="text/javascript"></script>
+        <script src="https://cdn.ckeditor.com/4.16.1/standard/ckeditor.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+
+        <script>
+    function readURL(input, thumbimage) {
+      if (input.files && input.files[0]) { //Sử dụng  cho Firefox - chrome
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $("#thumbimage").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+      else { // Sử dụng cho IE
+        $("#thumbimage").attr('src', input.value);
+      }
+      $("#thumbimage").show();
+      $('.filename').text($("#uploadfile").val());
+      $('.Choicefile').css('background', '#14142B');
+      $('.Choicefile').css('cursor', 'default');
+      $(".removeimg").show();
+      $(".Choicefile").unbind('click');
+
+    }
+    $(document).ready(function () {
+      $(".Choicefile").bind('click', function () {
+        $("#uploadfile").click();
+
+      });
+      $(".removeimg").click(function () {
+        $("#thumbimage").attr('src', '').hide();
+        $("#myfileupload").html('<input type="file" id="uploadfile"  onchange="readURL(this);" />');
+        $(".removeimg").hide();
+        $(".Choicefile").bind('click', function () {
+          $("#uploadfile").click();
+        });
+        $('.Choicefile').css('background', '#14142B');
+        $('.Choicefile').css('cursor', 'pointer');
+        $(".filename").text("");
+      });
+    })
+    function readURL2(input, thumbimage2) {
+      if (input.files && input.files[0]) { //Sử dụng  cho Firefox - chrome
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $("#thumbimage2").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+      else { // Sử dụng cho IE
+        $("#thumbimage2").attr('src', input.value);
+      }
+      $("#thumbimage2").show();
+      $('.filename').text($("#uploadfile2").val());
+      $('.Choicefile').css('background', '#14142B');
+      $('.Choicefile').css('cursor', 'default');
+      $(".removeimg").show();
+      $(".Choicefile").unbind('click');
+
+    }
+    $(document).ready(function () {
+      $(".Choicefile").bind('click', function () {
+        $("#uploadfil2").click();
+
+      });
+      $(".removeimg").click(function () {
+        $("#thumbimage2").attr('src', '').hide();
+        $("#myfileupload").html('<input type="file" id="uploadfile2"  onchange="readURL(this);" />');
+        $(".removeimg").hide();
+        $(".Choicefile").bind('click', function () {
+          $("#uploadfile2").click();
+        });
+        $('.Choicefile').css('background', '#14142B');
+        $('.Choicefile').css('cursor', 'pointer');
+        $(".filename").text("");
+      });
+    })
+    function readURLsp1(input, thumbimagesp1) {
+      if (input.files && input.files[0]) { //Sử dụng  cho Firefox - chrome
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $("#thumbimagesp1").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+      else { // Sử dụng cho IE
+        $("#thumbimagesp1").attr('src', input.value);
+      }
+      $("#thumbimagesp1").show();
+      $('.filename').text($("#uploadfilesp1").val());
+      $('.Choicefile').css('background', '#14142B');
+      $('.Choicefile').css('cursor', 'default');
+      $(".removeimg").show();
+      $(".Choicefile").unbind('click');
+
+    }
+    $(document).ready(function () {
+      $(".Choicefile").bind('click', function () {
+        $("#uploadfilesp1").click();
+
+      });
+      $(".removeimg").click(function () {
+        $("#uploadfilesp1").attr('src', '').hide();
+        $("#uploadfilesp1").html('<input type="file" id="uploadfilesp1"  onchange="readURL(this);" />');
+        $(".removeimg").hide();
+        $(".Choicefile").bind('click', function () {
+          $("#uploadfilesp1").click();
+        });
+        $('.Choicefile').css('background', '#14142B');
+        $('.Choicefile').css('cursor', 'pointer');
+        $(".filename").text("");
+      });
+    })
+    function readURLsp2(input, thumbimagesp2) {
+      if (input.files && input.files[0]) { //Sử dụng  cho Firefox - chrome
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $("#thumbimagesp2").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+      else { // Sử dụng cho IE
+        $("#thumbimagesp2").attr('src', input.value);
+      }
+      $("#thumbimagesp2").show();
+      $('.filename').text($("#uploadfilesp2").val());
+      $('.Choicefile').css('background', '#14142B');
+      $('.Choicefile').css('cursor', 'default');
+      $(".removeimg").show();
+      $(".Choicefile").unbind('click');
+
+    }
+    $(document).ready(function () {
+      $(".Choicefile").bind('click', function () {
+        $("#uploadfilesp2").click();
+
+      });
+      $(".removeimg").click(function () {
+        $("#uploadfilesp2").attr('src', '').hide();
+        $("#uploadfilesp2").html('<input type="file" id="uploadfilesp2"  onchange="readURL(this);" />');
+        $(".removeimg").hide();
+        $(".Choicefile").bind('click', function () {
+          $("#uploadfilesp2").click();
+        });
+        $('.Choicefile').css('background', '#14142B');
+        $('.Choicefile').css('cursor', 'pointer');
+        $(".filename").text("");
+      });
+    })
+    function readURLsp3(input, thumbimagesp3) {
+      if (input.files && input.files[0]) { //Sử dụng  cho Firefox - chrome
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $("#thumbimagesp3").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+      else { // Sử dụng cho IE
+        $("#thumbimagesp3").attr('src', input.value);
+      }
+      $("#thumbimagesp3").show();
+      $('.filename').text($("#uploadfilesp3").val());
+      $('.Choicefile').css('background', '#14142B');
+      $('.Choicefile').css('cursor', 'default');
+      $(".removeimg").show();
+      $(".Choicefile").unbind('click');
+
+    }
+    $(document).ready(function () {
+      $(".Choicefile").bind('click', function () {
+        $("#uploadfilesp3").click();
+
+      });
+      $(".removeimg").click(function () {
+        $("#uploadfilesp3").attr('src', '').hide();
+        $("#uploadfilesp3").html('<input type="file" id="uploadfilesp3"  onchange="readURL(this);" />');
+        $(".removeimg").hide();
+        $(".Choicefile").bind('click', function () {
+          $("#uploadfilesp3").click();
+        });
+        $('.Choicefile').css('background', '#14142B');
+        $('.Choicefile').css('cursor', 'pointer');
+        $(".filename").text("");
+      });
+    })
+    function readURLsp4(input, thumbimagesp4) {
+      if (input.files && input.files[0]) { //Sử dụng  cho Firefox - chrome
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $("#thumbimagesp4").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+      else { // Sử dụng cho IE
+        $("#thumbimagesp4").attr('src', input.value);
+      }
+      $("#thumbimagesp4").show();
+      $('.filename').text($("#uploadfilesp4").val());
+      $('.Choicefile').css('background', '#14142B');
+      $('.Choicefile').css('cursor', 'default');
+      $(".removeimg").show();
+      $(".Choicefile").unbind('click');
+
+    }
+    $(document).ready(function () {
+      $(".Choicefile").bind('click', function () {
+        $("#uploadfilesp4").click();
+
+      });
+      $(".removeimg").click(function () {
+        $("#uploadfilesp4").attr('src', '').hide();
+        $("#uploadfilesp4").html('<input type="file" id="uploadfilesp4"  onchange="readURL(this);" />');
+        $(".removeimg").hide();
+        $(".Choicefile").bind('click', function () {
+          $("#uploadfilesp4").click();
+        });
+        $('.Choicefile').css('background', '#14142B');
+        $('.Choicefile').css('cursor', 'pointer');
+        $(".filename").text("");
+      });
+    })
+    
+  </script>
+      </head>
+
+<body onload="time()" class="app sidebar-mini rtl">
+  <!-- Navbar-->
+  <header class="app-header">
+    <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar"
+      aria-label="Hide Sidebar"></a>
+    <!-- Navbar Right Menu-->
+    <ul class="app-nav">
+
+
+      <!-- User Menu-->
+      <li><a class="app-nav__item" href="../index.php"><i class='bx bx-log-out bx-rotate-180'></i> </a>
+
+      </li>
+    </ul>
+  </header>
+  <!-- Sidebar menu-->
+  <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+  <aside class="app-sidebar">
+    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar"
+        src="../view/layout/images/menu/logo/logoam.png" width="50px" alt="User Image">
+      <div>
+        <p class="app-sidebar__user-name"><b>Admin Techpro</b></p>
+        <p class="app-sidebar__user-designation">Chào mừng bạn trở lại</p>
+      </div>
+    </div>
+    <hr>
+    <ul class="app-menu">
+      <li><a class="app-menu__item haha" href="phan-mem-ban-hang.html"><i class='app-menu__icon bx bx-cart-alt'></i>
+          <span class="app-menu__label">POS Bán Hàng</span></a></li>
+      <li><a class="app-menu__item active" href="index.php"><i class='app-menu__icon bx bx-tachometer'></i><span
+            class="app-menu__label">Bảng điều khiển</span></a></li>
+      <li>
+        <a class="app-menu__item" href="index.php?pg=qldanhmuc">
+          <i class='app-menu__icon bi bi-card-checklist'></i>
+          <span class="app-menu__label">Quản lý danh mục</span>
+        </a>
+      </li>
+      <li>
+        <a class="app-menu__item" href="index.php?pg=qldanhmuc">
+          <!-- <i class='app-menu__icon bi bi-card-checklist'></i> -->
+          <i class="app-menu__icon fa-brands fa-bandcamp"></i>
+          <span class="app-menu__label">Quản lý thương hiệu</span>
+        </a>
+      </li>
+      <li>
+        <a class="app-menu__item" href="index.php?pg=qlsanpham">
+          <i class='app-menu__icon bx bx-purchase-tag-alt'></i>
+          <span class="app-menu__label">Quản lý sản phẩm</span>
+        </a>
+      </li>
+      <li><a class="app-menu__item" href="index.php?pg=qldonhang"><i class='app-menu__icon bx bx-task'></i><span
+            class="app-menu__label">Quản lý đơn hàng</span></a></li>
+      <li class="app-menu__itemhv">
+        <a class="app-menu__item" href="">
+          <i class='app-menu__icon bx bx-image'></i>
+          <span class="app-menu__label">Quản lý giao diện</span>
+        </a>
+        <ul class="sub-menu">
+          <li><a class="app-menu__item" href="#"><i class='app-menu__icon bi bi-headset-vr'></i><span
+                class="app-menu__label">Header</span></a></li>
+          <li><a class="app-menu__item" href="#"><i class='app-menu__icon bi bi-back'></i><span
+                class="app-menu__label">Footer</span></a></li>
+          <li><a class="app-menu__item" href="#"><i class='app-menu__icon bx bx-toggle-right'></i><span
+                class="app-menu__label">Button</span></a></li>
+          <li><a class="app-menu__item" href="index.php?pg=qlbanner"><i class='app-menu__icon bx bx-slideshow'></i><span
+                class="app-menu__label">Banner</span></a></li>
+        </ul>
+      </li>
+      <li><a class="app-menu__item" href="#"><i class='app-menu__icon bx bx-cog'></i><span class="app-menu__label">Cài
+            đặt hệ thống</span></a></li>
+    </ul>
+  </aside>
---
+++ admin/view/updatesp.php
@@ -0,0 +1,347 @@
+<?php
+if(is_array($showup)&&(count($showup)>0)){
+  extract($showup);
+  $idupdate= $id;
+  }
+
+
+  $html_showbradm = '';
+  foreach ($dsbr_adm as $br_adm) {
+      extract($br_adm);
+      if($id==$id_brand){
+        $html_showbradm .= '<option selected value="'.$id.'">'.$ten.'</option>';
+      }else{  
+        $html_showbradm .= '<option value="'.$id.'">'.$ten.'</option>';
+      }
+  }
+  $html_showdmadm = '';
+  foreach ($dsdm_adm as $dm_adm) {
+      extract($dm_adm);
+      if($id==$id_catalog){
+        $html_showdmadm .= '<option selected value="'.$id.'">'.$ten_dm.'</option>';
+      }else{
+        $html_showdmadm .= '<option value="'.$id.'">'.$ten_dm.'</option>';
+      }
+  }
+  
+?>
+?>
+<main class="app-content">
+    <div class="app-title">
+      <ul class="app-breadcrumb breadcrumb side">
+        <li class="breadcrumb-item">Quản lý sản phẩm</li>
+        <li class="breadcrumb-item active"><a href="#"></a>Cập nhật sản phẩm</li>
+      </ul>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="tile">
+          <h3 class="tile-title">Cập nhật sản phẩm</h3>
+          <div class="tile-body">
+          </div>
+          <div class="formsp">
+            <form action="index.php?pg=updatepro" enctype="multipart/form-data" method="post" class="row">
+              <div class="form-group col-md-3">
+                <label for="masp" class="control-label">Mã sản phẩm <span style="color: red; font-weight: bold" >(*)</span>  </label>
+                <input class="form-control" id="masp" name="masp" type="text" value="<?=$ma_sp?>">
+              </div>
+              <div class="form-group col-md-3">
+                <label for="tensp" class="control-label">Tên sản phẩm <span style="color: red; font-weight: bold" >(*)</span> </label>
+                <input class="form-control" id="tensp" name="tensp" type="text" value="<?=($ten!="")?$ten:"";?>">
+              </div>
+              <div class="form-group col-md-3">
+                <label for="dmsp" class="control-label">Danh mục <span style="color: red; font-weight: bold" >(*)</span> </label>
+                <select id="dmsp" class="form-control" name="danhmucsp">
+                  <option value="default" >--Chọn danh mục--</option>
+                  <?=$html_showdmadm?>
+                </select>
+              </div>
+              <div class="form-group col-md-3 ">
+                <label for="brandsp" class="control-label">Brand<span style="color: red; font-weight: bold" >(*)</span> </label>
+                <select class="form-control" name="brandsp" id="brandsp">
+                  <option value="default" >-- Chọn hãng --</option>
+                  <?=$html_showbradm?>
+                </select>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="giaban" class="control-label">Giá bán<span style="color: red; font-weight: bold" >(*)</span> </label>
+                <input class="form-control" id="giaban" name="giaban" value="<?=$gia?>" type="text">
+              </div>
+              <div class="form-group col-md-3">
+                <label for="giagiamsp" class="control-label" for="giagiamsp">Giá giảm</label>
+                <input id="giagiamsp" class="form-control" id="giagiamsp" name="giagiam" value="<?=$giamgia?>" type="text">
+              </div>
+              <div class="form-group col-md-3 ">
+                <label for="tinhtrangsp" class="control-label">Tình trạng</label>
+                <select class="form-control" id="tinhtrangsp">
+                  <option>-- Chọn tình trạng --</option>
+                  <option>Còn hàng</option>
+                  <option>Hết hàng</option>
+                </select>
+              </div>
+              <div class="form-group col-md-3">
+
+              </div>
+              <div class="form-group col-md-3">
+                <label class="control-label">Thuộc tính <span style="color: red; font-weight: bold" >(*)</span></label>
+                <div class="col">
+                  <label for=""><input type="checkbox" name="seo" value="<?=$sale;?>" id=""> Sale</label>
+                </div>
+                <div class="col">
+                  <label for=""><input type="checkbox" name="moi" value="<?=$new;?>" id=""> Mới</label>
+                </div>
+                <div class="col">
+                  <label for=""><input type="checkbox" name="many" value="<?=$xemnhieu;?>" id=""> Xem nhiều</label>
+                </div>
+                <div class="col">
+                  <label for=""><input type="checkbox" name="run" value="<?=$banchay;?>" id=""> Bán chạy</label>
+                </div>
+              </div>
+              <div class="form-group col-md-9">
+                <label class="control-label">Ảnh sản phẩm<span style="color: red; font-weight: bold" >(*)</span> </label>
+                <div id="myfileupload">
+                  <input type="file" id="uploadfile" name="imgup" value="<?=$hinh;?>" onchange="readURL(this);" />
+                  <input type="hidden" id="uploadfile" name="imgold" value="<?=$hinh;?>" />
+                  <img style="width: 200px;" src="../view/layout/images/product/<?=$hinh?>" alt="">
+                </div>
+                <div id="thumbbox">
+                  <img width="200" alt="Thumb image" id="thumbimage" style="display: none" />
+                  <a class="removeimg" href="javascript:"></a>
+                </div>
+                <div id="boxchoice">
+                  <a href="javascript:" class="Choicefile"><i class="fas fa-cloud-upload-alt"></i> Chọn ảnh</a>
+                  <p style="clear:both"></p>
+                </div>
+              </div>
+                <p class="tile-title col-md-12">Ảnh chi tiết sản phẩm</p>
+                <div class="col-md-3">
+                  <p class="control-label">Ảnh 1</p>
+                  <img style="width: 70px;"  src="../view/layout/images/product/<?=$hinh1?>" alt="">
+                  <input type="hidden" id="uploadfile1" name="imgold1" value="<?=$hinh1;?>" />
+                  <input type="file" onchange="readURLsp1(this);" id="uploadfile1" name="hinh1"/>
+                  <div id="thumbboxsp1">
+                    <img width="70" alt="Thumb image" placeholder="ảnh tải lên" id="thumbimagesp1" style="display: none" />
+                    <a class="removeimg" href="javascript:"></a>
+                  </div>
+                </div><br>
+                <div class="col-md-3">
+                  <p class="control-label">Ảnh 2</p>
+                  <img style="width: 70px;" src="../view/layout/images/product/<?=$hinh2?>" alt="">
+                  <input type="hidden" id="uploadfile2" name="imgold2" value="<?=$hinh2;?>" />
+                  <input type="file" onchange="readURLsp2(this)" id="uploadfile2" name="hinh2"/>
+                  <div id="thumbboxsp2">
+                    <img width="70" alt="Thumb image" id="thumbimagesp2" style="display: none" />
+                    <a class="removeimg" href="javascript:"></a>
+                  </div>
+                </div><br>
+                <div class="col-md-3">
+                  <p class="control-label">Ảnh 3</p>
+                  <img style="width: 70px;" src="../view/layout/images/product/<?=$hinh3?>" alt="">
+                  <input type="hidden" id="uploadfile3" name="imgold3" value="<?=$hinh3;?>" />
+                  <input type="file" onchange="readURLsp3(this)" id="uploadfile3" name="hinh3"/>
+                  <div id="thumbboxsp3">
+                    <img width="70" alt="Thumb image" id="thumbimagesp3" style="display: none" />
+                    <a class="removeimg" href="javascript:"></a>
+                  </div>
+                </div><br>
+                <div class="col-md-3">
+                  <p class="control-label">Ảnh 4</p>
+                  <img style="width: 70px;" src="../view/layout/images/product/<?=$hinh4?>" alt="">
+                  <input type="hidden" id="uploadfile4" name="imgold4" value="<?=$hinh4;?>" />
+                  <input type="file" onchange="readURLsp4(this)" id="uploadfile4" name="hinh4"/>
+                  <div id="thumbboxsp4">
+                    <img width="70" alt="Thumb image" id="thumbimagesp4" style="display: none" />
+                    <a class="removeimg" href="javascript:"></a>
+                  </div>
+                </div><br>
+              <div class="form-group col-md-12">
+                <br>
+                <label class="control-label">Mô tả</label>
+                <input class="form-control" name="mota" value="<?=$mota?>" type="text">
+              </div>
+              <div class="form-group col-md-12">
+                <label class="control-label">Chi tiết sản phẩm</label>
+                <textarea class="form-control" name="chitiet" id="chitiet"><?=$chitiet?></textarea>
+                <script>CKEDITOR.replace('chitiet');</script>
+              </div>
+              <input class="btn btn-save" type="submit" name="updatepro" value="Lưu lại">
+              <a class="btn btn-cancel" href="qlsanpham.html">Hủy bỏ</a>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+  <!--
+  MODAL CHỨC VỤ 
+-->
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+
+        <div class="modal-body">
+          <div class="row">
+            <div class="form-group  col-md-12">
+              <span class="thong-tin-thanh-toan">
+                <h5>Thêm mới nhà cung cấp</h5>
+              </span>
+            </div>
+            <div class="form-group col-md-12">
+              <label class="control-label">Nhập tên chức vụ mới</label>
+              <input class="form-control" type="text" required>
+            </div>
+          </div>
+          <BR>
+          <button class="btn btn-save" type="button">Lưu lại</button>
+          <a class="btn btn-cancel" data-dismiss="modal" href="#">Hủy bỏ</a>
+          <BR>
+        </div>
+        <div class="modal-footer">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--
+MODAL
+-->
+
+
+
+  <!--
+  MODAL DANH MỤC
+-->
+  <div class="modal fade" id="adddanhmuc" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+
+        <div class="modal-body">
+          <div class="row">
+            <div class="form-group  col-md-12">
+              <span class="thong-tin-thanh-toan">
+                <h5>Thêm mới danh mục </h5>
+              </span>
+            </div>
+            <div class="form-group col-md-12">
+              <label class="control-label">Nhập tên danh mục mới</label>
+              <input class="form-control" type="text" required>
+            </div>
+            <div class="form-group col-md-12">
+              <label class="control-label">Danh mục sản phẩm hiện đang có</label>
+              <ul style="padding-left: 20px;">
+                <li>Bàn ăn</li>
+              <li>Bàn thông minh</li>
+              <li>Tủ</li>
+              <li>Ghế gỗ</li>
+              <li>Ghế sắt</li>
+              <li>Giường người lớn</li>
+              <li>Giường trẻ em</li>
+              <li>Bàn trang điểm</li>
+              <li>Giá đỡ</li>
+              </ul>
+            </div>
+          </div>
+          <BR>
+          <button class="btn btn-save" type="button">Lưu lại</button>
+          <a class="btn btn-cancel" data-dismiss="modal" href="#">Hủy bỏ</a>
+          <BR>
+        </div>
+        <div class="modal-footer">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--
+MODAL
+-->
+
+
+
+
+  <!--
+  MODAL TÌNH TRẠNG
+-->
+  <div class="modal fade" id="addtinhtrang" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+
+        <div class="modal-body">
+          <div class="row">
+            <div class="form-group  col-md-12">
+              <span class="thong-tin-thanh-toan">
+                <h5>Thêm mới tình trạng</h5>
+              </span>
+            </div>
+            <div class="form-group col-md-12">
+              <label class="control-label">Nhập tình trạng mới</label>
+              <input class="form-control" type="text" required>
+            </div>
+          </div>
+          <BR>
+          <button class="btn btn-save" type="button">Lưu lại</button>
+          <a class="btn btn-cancel" data-dismiss="modal" href="#">Hủy bỏ</a>
+          <BR>
+        </div>
+        <div class="modal-footer">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--
+MODAL
+-->
+<script>
+  var form = document.getElementsByTagName("form")[0];
+  var masp = document.getElementById("masp");
+  var tensp = document.getElementById("tensp");
+  var danhmucsp = document.getElementById("dmsp");
+  var brandsp = document.getElementById("brandsp");
+  var giaban = document.getElementById("giaban");
+  var giagiamsp = document.getElementById("giagiamsp");
+  console.log(giaban);
+  form.addEventListener("submit", function(event){
+    if(masp.value == "" || masp.value.length>10){
+      alert("Mã sản phẩm không được để trống tối đa 10 ký tự!");
+      event.preventDefault();// không cho submit
+      masp.focus();//di chuyển đến vị trí lỗi
+      return false;
+    }
+    if(tensp.value == ""){
+      alert("Tên sản phẩm không được để trống!");
+      event.preventDefault();// không cho submit
+      tensp.focus();//di chuyển đến vị trí lỗi
+      return false;
+    }
+    if (!(danhmucsp && danhmucsp.value !== "default")) { // Kiểm tra giá trị chọn của danh mục
+        alert("Danh mục không được để trống!");
+        event.preventDefault();
+        danhmucsp.focus();
+        return false;
+    }
+    if(giaban.value == "" || isNaN(giaban.value)){
+      alert("Giá bán không được để trống và phải là số!");
+      event.preventDefault();// không cho submit
+      giaban.focus();//di chuyển đến vị trí lỗi
+      return false;
+    }
+    if(giagiamsp.value == "" || isNaN(giagiamsp.value)){
+      alert("Giá giảm không được để trống và phải là số!");
+      event.preventDefault();// không cho submit
+      giagiamsp.focus();//di chuyển đến vị trí lỗi
+      return false;
+    }
+    if (!(brandsp && brandsp.value !== "default")) { // Kiểm tra giá trị chọn của brand
+        alert("Brand không được để trống!");
+        event.preventDefault();
+        brandsp.focus();
+        return false;
+    }
+
+    return true;
+  });
+
+</script>
