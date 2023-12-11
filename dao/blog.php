ed59db375ba89aacf234f8077d221ac50ee722a5
hothinh191 <hongocthinh1912004@gmail.com>
2023-12-11 08:32:38 +0700

    thinh
    blog dao

--- dao/blog.php
+++ dao/blog.php
@@ -1,11 +1,11 @@
 <?php
 // hiển thị tương tác với bảng blog
 require_once 'pdo.php';
-function get_tintucs($iddm = 0, $start = 0, $limit = 0)
+function get_tintucs($madm = 0, $start = 0, $limit = 0)
 {
     $sql = "SELECT * FROM blog p INNER JOIN catalog c ON p.madm = c.id";
-    if ($iddm != 0) {
-        $sql .= " WHERE p.iddm = " . $iddm;
+    if ($madm != 0) {
+        $sql .= " WHERE p.madm = " . $madm;
     }
     if ($limit != 0) {
         $sql .= " LIMIT " . $start . "," . $limit;
@@ -13,7 +13,7 @@ function get_tintucs($iddm = 0, $start = 0, $limit = 0)
     $conn = pdo_get_connection();
     $stmt = $conn->prepare($sql);
     $stmt->execute();
-    return $stmt->fetchALL(); // lấy all dữ liệu
+    return $stmt->fetchALL(); // lấy tất cả các dòng kết quả từ câu truy vấn
 }
 // tương tác với trang CTSP
 function get_tintuc($id)
@@ -31,7 +31,7 @@ function count_tintuc()
     $conn = pdo_get_connection();
     $stmt = $conn->prepare($sql);
     $stmt->execute();
-    return $stmt->fetch(); //chỉ lấy 1 dữ liệu
+    return $stmt->fetch(); //lấy một dòng kết quả từ câu truy vấn
 }
 // tương tác với bảng catalog và hiển thị danh mục sp
 function get_catogories($iddm)
@@ -40,7 +40,7 @@ function get_catogories($iddm)
     if ($iddm > 0) {
         $sql .= "AND iddm=" . $iddm;
     }
-    $sql .= " ORDER BY stt DESC  ";
+    $sql .= " ORDER BY stt DESC  "; //giới hạn kết quả chỉ chứa các danh mục có iddm tương ứng với giá trị của biến $iddm.
     $conn = pdo_get_connection();
     $stmt = $conn->prepare($sql);
     $stmt->execute();
